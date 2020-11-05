0cc3948104c921f534a396e3aed9fe6618cf68df
Marko Strisko <markostrisko@codetri.be>
2020-11-05 13:49:20 +0100

    Initial commit with starting project structure

---
+++ src/test/java/com/flight/advisor/AdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.advisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
